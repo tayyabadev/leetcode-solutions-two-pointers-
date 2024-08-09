a8084686a300edf5e12eb3d16357dbf0f2c5e3ff
Tayyaba Tabassum <142013549+tayyabadev@users.noreply.github.com>
2024-08-09 11:47:18 +0500

    Time: 4 ms (69.45%) | Memory: 20.6 MB (56.85%) - LeetSync

--- 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.cpp
+++ 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     vector<int> findIndices(vector<int>& nums, int indexDifference,
                             int valueDifference) {
-        int left = 0;
-        int right = 0;
+
         for (int left = 0; left < nums.size(); left++) {
             for (int right = left; right < nums.size(); right++) {
                 if (abs(left - right) >= indexDifference &&
                     abs(nums[left] - nums[right]) >= valueDifference) {
                     return {left, right};
-                } 
+                }
             }
-            
-            }    return {-1, -1};
         }
-    };
+        return {-1, -1};
+    }
+};
