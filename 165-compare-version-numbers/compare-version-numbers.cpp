af46d7b796b56dac4e1491c661cb2de62cabf276
Tayyaba Tabassum <142013549+tayyabadev@users.noreply.github.com>
2024-08-11 12:24:35 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (46.00%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int ptr1 = 0;
+        int ptr2 = 0;
+        int size1 = version1.size();
+        int size2 = version2.size();
+       
+
+        while (ptr1 < size1 || ptr2 < size2) {
+             int num1 = 0, num2 = 0;
+            while (ptr1 < size1 && version1[ptr1] != '.') {
+               num1 = num1 * 10 + (version1[ptr1] - '0');
+                ptr1++;
+            }
+            while (ptr2 < size2 && version2[ptr2] != '.') {
+               num2 = num2 * 10 + (version2[ptr2] - '0');
+                ptr2++;
+            }
+            if (num1 < num2) {
+                return -1;
+            }
+            if (num1 > num2) {
+                return 1;
+            }
+            ptr1++;
+            ptr2++;
+
+        }
+        return 0;
+    }
+};
