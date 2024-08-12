c3820b5901c053e69368485b76534b2899cc6cda
Tayyaba Tabassum <142013549+tayyabadev@users.noreply.github.com>
2024-08-12 08:32:53 +0500

    Time: 3 ms (85.81%) | Memory: 8.8 MB (18.14%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     string reverseStr(string s, int k) {
-         int n = s.size();
-         
-        for (int start = 0; start < n; start += 2 * k) {
-            int left = start;
-            int right = min(start + k - 1,
-                            n - 1); // Reverse only up to the end of the string
+        int n = s.size();
 
+        for (int i = 0; i < n; i += 2 * k) {
+            int left = i;
+            int right = min(i + k - 1, n - 1);
             // Reverse the first k characters in the current chunk
             while (left < right) {
                 swap(s[left], s[right]);
