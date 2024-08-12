daec1cceb63b75f4ccbb573bab4b2c7c7dbb4d55
Tayyaba Tabassum <142013549+tayyabadev@users.noreply.github.com>
2024-08-12 14:45:22 +0500

    Time: 7 ms (35.27%) | Memory: 8.7 MB (44.66%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -5,8 +5,7 @@ public:
 
         for (int i = 0; i < n; i += 2 * k) {
             int left = i;
-            int right = min(i + k - 1, n - 1);
-            // Reverse the first k characters in the current chunk
+            int right = min(left + k - 1, n - 1);
             while (left < right) {
                 swap(s[left], s[right]);
                 left++;
