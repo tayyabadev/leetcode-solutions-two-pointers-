7fd216ab7239b6ad8e3fd565db429ab7d19d6ba0
Tayyaba Tabassum <142013549+tayyabadev@users.noreply.github.com>
2024-08-14 14:34:12 +0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (17.06%) - LeetSync

--- 841-shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
         int size = s.size();
-        vector<int>arr(size,0);
+        vector<int>arr;
         int pos =INT_MAX;
         for (int i = 0; i < size; i++) {
             if (s[i] == c) {
                 pos = i;
             }
-            arr[i] = abs(pos - i);
+            arr.push_back(abs(pos - i));
         }
         for (int i = pos; i >= 0; i--) {
             if (s[i] == c) {
