c8c043661dcf194d92636110ab7bced05923bea8
Tayyaba Tabassum <142013549+tayyabadev@users.noreply.github.com>
2024-08-16 23:51:06 +0500

    Time: 13 ms (5.87%) | Memory: 15.1 MB (6.50%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_map<int, int> map;
+        for (int i=0;i<arr.size();i++) {
+            map[arr[i]] = i;
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            if (map.find(arr[i] * 2) != map.end() && map[arr[i]*2]!=i) {
+                return true;
+            }
+        }
+        return false;
+      
+    }
+};
