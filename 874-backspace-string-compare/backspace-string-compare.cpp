c1935d6827d5775d6551457420afdfedfec2bed9
Tayyaba Tabassum <142013549+tayyabadev@users.noreply.github.com>
2024-08-16 15:59:35 +0500

    Time: 0 ms (100.00%) | Memory: 7.9 MB (55.90%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool backspaceCompare(string s, string t) {
+    string check(string s) {
         string first = "";
         int count = 0;
         for (int i = s.size() - 1; i >= 0; i--) {
@@ -14,18 +14,7 @@ public:
                 first += s[i];
             }
         }
-        string sec = "";
-        count = 0;
-        for (int i = t.size() - 1; i >= 0; i--) {
-            if (t[i] == '#') {
-                count++;
-                continue;
-            }
-            if (count > 0) {
-                count--;
-            } else {
-                sec += t[i];
-            }
-        }return first==sec;
+        return first;
     }
+    bool backspaceCompare(string s, string t) { return check(s) == check(t); }
 };
