74aee188e63b38e922b0b81a9b408c73b68ec582
Tayyaba Tabassum <142013549+tayyabadev@users.noreply.github.com>
2024-08-18 19:42:46 +0500

    Time: 39 ms (8.00%) | Memory: 10.7 MB (72.94%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -3,8 +3,8 @@ public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
         int count = 0;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums.size(); j++) {
-                for (int k = 0; k < nums.size(); k++) {
+            for (int j = i; j < nums.size(); j++) {
+                for (int k = j; k < nums.size(); k++) {
                     if (i < j < k && nums[j] - nums[i] == diff &&
                         nums[k] - nums[j] == diff) {
                         count++;
